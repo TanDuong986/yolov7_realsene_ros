0beb00c405ce94a6d84f3985ac5c95431787c107
TanDuong986 <duongtanrb@gmail.com>
2023-07-03 01:00:20 +0700

    change function attempt_download in line 31 to adapt with name of weight

--- models/experimental.py
+++ models/experimental.py
@@ -240,10 +240,6 @@ class End2End(nn.Module):
         x = self.end2end(x)
         return x
 
-
-
-
-
 def attempt_load(weights, map_location=None):
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
     model = Ensemble()
